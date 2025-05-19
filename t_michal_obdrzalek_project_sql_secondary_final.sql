5f984efda25c847bea73058a2cc1b8bed5d0615c
raz8x <michal.obdrzalek@seznam.cz>
2025-05-19 11:56:12 +0200

    Update t_michal_obdrzalek_project_sql_secondary_final.sql
    Vizualni upravy

--- t_michal_obdrzalek_project_sql_secondary_final.sql
+++ t_michal_obdrzalek_project_sql_secondary_final.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS t_michal_obdrzalek_project_SQL_secondary_final;
 -- spojeni tabulek economies a countries na zaklade statu, predvybrany odpovidajici roky pro ukol c.5
 
 CREATE TABLE t_michal_obdrzalek_project_SQL_secondary_final AS
-
 SELECT e.country,
 	e.year,
 	e.GDP,
@@ -13,14 +12,4 @@ FROM economies e
 JOIN countries c ON e.country = c.country
 WHERE e.year BETWEEN 2006 AND 2018
   AND c.continent = 'Europe'
-ORDER BY e.country, e.year
-
-
-
-
-
-
-
-
-
-
+ORDER BY e.country, e.year;
